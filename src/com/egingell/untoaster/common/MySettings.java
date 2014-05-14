b515f1726f6805aee867775887e9f49f7ae86775
egingell <egingell@gmail.com>
2014-05-14 17:41:43 -0600

    * Xposed v52 had issues with my module's constructor. I don't know why it suddenly cropped up, but there you have it.

--- src/com/egingell/untoaster/common/MySettings.java
+++ src/com/egingell/untoaster/common/MySettings.java
@@ -3,12 +3,12 @@
  *
  * Copyright 2014 Eric Gingell (c)
  *
- *     ButteredToast is free software: you can redistribute it and/or modify
+ *     UnToasted is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
  *
- *     ButteredToast is distributed in the hope that it will be useful,
+ *     UnToasted is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
@@ -23,7 +23,6 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import de.robv.android.xposed.XSharedPreferences;
-
 
 public class MySettings {
 	
--- src/com/egingell/untoaster/xposed/Hook.java
+++ src/com/egingell/untoaster/xposed/Hook.java
@@ -3,12 +3,12 @@
  *
  * Copyright 2014 Eric Gingell (c)
  *
- *     ButteredToast is free software: you can redistribute it and/or modify
+ *     UnToasted is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
  *
- *     ButteredToast is distributed in the hope that it will be useful,
+ *     UnToasted is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
@@ -16,12 +16,13 @@
  *     You should have received a copy of the GNU General Public License
  *     along with UnToasted.  If not, see <http://www.gnu.org/licenses/>.
  *     
- *     Xposed log: tail -f -n 100 /data/data/de.robv.android.xposed.installer/log/debug.log  >/sdcard/UnToaster.log
+ *     Xposed log: tail -f -n 100 /data/data/de.robv.android.xposed.installer/log/error.log  >/sdcard/UnToaster.log
  *     Logcat: logcat | grep "UnToaster"
  */
 
 package com.egingell.untoaster.xposed;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.regex.Pattern;
@@ -39,6 +40,7 @@ import android.widget.Toast;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
@@ -46,7 +48,7 @@ public class Hook implements IXposedHookLoadPackage, IXposedHookZygoteInit {
 
 	//private static String PATH = null;
 	private HashMap<String,MySettings> prefs = new HashMap<String,MySettings>();
-	private Hook() {}
+	public Hook() {}
 
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
@@ -56,86 +58,96 @@ public class Hook implements IXposedHookLoadPackage, IXposedHookZygoteInit {
 
     @Override
     public void initZygote(StartupParam startupParam) throws Throwable {
-    	XC_MethodHook hook = new XC_MethodHook() {
-			final private Pattern splitPattern = Pattern.compile("[\r\n]+");
-		    private final HashMap<String,Pattern> patternCache = new HashMap<String,Pattern>();
-		    private boolean filter(String needle, String haystack) {
-			    if (! patternCache.containsKey(needle)) {
-			 	   patternCache.put(needle, Pattern.compile(needle));
+    	try {
+	    	XC_MethodHook hook = new XC_MethodHook() {
+				final private Pattern splitPattern = Pattern.compile("[\r\n]+");
+			    private final HashMap<String,Pattern> patternCache = new HashMap<String,Pattern>();
+			    private boolean filter(String needle, String haystack) {
+				    if (! patternCache.containsKey(needle)) {
+				 	   patternCache.put(needle, Pattern.compile(needle));
+				    }
+				    boolean b = patternCache.get(needle).matcher(haystack).find();
+				    //Util.log("UnToaster#filter: checking\n\tpattern " + needle + "\n\tin " + haystack + "\n\tresult " + (b ? "true" : "false"));
+				    return b;
 			    }
-			    boolean b = patternCache.get(needle).matcher(haystack).find();
-			    //Util.log("UnToaster#filter: checking\n\tpattern " + needle + "\n\tin " + haystack + "\n\tresult " + (b ? "true" : "false"));
-			    return b;
-		    }
-			@Override
-			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-				View layout = (View) ((Toast) param.thisObject).getView();
-				Context context = layout.getContext();
-				TextView view = (TextView) layout.findViewById(android.R.id.message);
-		    	boolean show = true;
-				try {
-					final PackageManager pm = context.getPackageManager();
-					String packageName = context.getPackageName();
-					String appName = pm.getApplicationLabel(context.getApplicationInfo()).toString();
-					ArrayList<String> list = new ArrayList<String>();
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 					try {
-						list.add(splitPattern.matcher(view.getText().toString()).replaceAll(" ").trim());
-					} catch (Throwable e) {}
-					int i = 0;
-					try {
-						do {
+						View layout = (View) ((Toast) param.thisObject).getView();
+						Context context = layout.getContext();
+						TextView view = (TextView) layout.findViewById(android.R.id.message);
+				    	boolean show = true;
+						try {
+							final PackageManager pm = context.getPackageManager();
+							String packageName = context.getPackageName();
+							String appName = pm.getApplicationLabel(context.getApplicationInfo()).toString();
+							ArrayList<String> list = new ArrayList<String>();
 							try {
-								list.add(splitPattern.matcher(((TextView) ((ViewGroup) layout).getChildAt(i++)).getText().toString()).replaceAll(" ").trim());
-							} catch (ClassCastException e) {}
-						} while (((ViewGroup) layout).getChildCount() > i);
+								list.add(splitPattern.matcher(view.getText().toString()).replaceAll(" ").trim());
+							} catch (Throwable e) {}
+							int i = 0;
+							try {
+								do {
+									try {
+										list.add(splitPattern.matcher(((TextView) ((ViewGroup) layout).getChildAt(i++)).getText().toString()).replaceAll(" ").trim());
+									} catch (ClassCastException e) {}
+								} while (((ViewGroup) layout).getChildCount() > i);
+							} catch (Throwable e) {
+								//Util.log("Unable to find a layout for " + packageName + " (" + appName + ")");
+							}
+							
+							MySettings settings = prefs.get(packageName), pAll = new MySettings("all"), appSettings = new MySettings("com.egingell.untoaster");
+							
+				    		appSettings.reload();
+				    		settings.reload();
+				    		pAll.reload();
+				    		
+							String blocked = "allowed";
+							
+							String all = pAll.get("content", "NA").trim();
+							String fPackage = settings.get("content", "NA").trim();
+					 		String patterns = "";
+					 		
+					 		if (! fPackage.equals("NA")) {
+					 			if (fPackage.equals("")) {
+					 				fPackage = ".*";
+					 			}
+						 		if (! all.equals("NA")) {
+						 			patterns += all + "\n" + fPackage;
+						 		} else {
+						 			patterns += fPackage;
+						 		}
+					 		} else if (! all.equals("NA")) {
+					 			patterns += all;
+					 		}
+					 		for (String content : list) {
+					 			if (! patterns.trim().equals("NA") && ! patterns.trim().equals("")) {
+						 			for (String s : splitPattern.split(patterns)) {
+										if (filter(s, content)) {
+											blocked = "blocked";
+											show = false;
+										}
+						 			}
+						 		}
+						 		Util.log(packageName + " (" + appName + ")\n\thaystack: " + content + "\n\tneedles (all): " + all + "\n\tneedles (" + packageName + "): " + fPackage + "\n\ttoast: " + blocked);
+					 		}
+						} catch (Throwable e) {
+							Util.log(e);
+						}
+				        if (!show) {
+				        	param.setResult(null);
+				        }
 					} catch (Throwable e) {
-						//Util.log("Unable to find a layout for " + packageName + " (" + appName + ")");
+						Util.log(e);
 					}
-					
-					MySettings settings = prefs.get(packageName), pAll = new MySettings("all"), appSettings = new MySettings("com.egingell.untoaster");
-					
-		    		appSettings.reload();
-		    		settings.reload();
-		    		pAll.reload();
-		    		
-					String blocked = "allowed";
-					
-					String all = pAll.get("content", "NA").trim();
-					String fPackage = settings.get("content", "NA").trim();
-			 		String patterns = "";
-			 		
-			 		if (! fPackage.equals("NA")) {
-			 			if (fPackage.equals("")) {
-			 				fPackage = ".*";
-			 			}
-				 		if (! all.equals("NA")) {
-				 			patterns += all + "\n" + fPackage;
-				 		} else {
-				 			patterns += fPackage;
-				 		}
-			 		} else if (! all.equals("NA")) {
-			 			patterns += all;
-			 		}
-			 		for (String content : list) {
-			 			if (! patterns.trim().equals("NA") && ! patterns.trim().equals("")) {
-				 			for (String s : splitPattern.split(patterns)) {
-								if (filter(s, content)) {
-									blocked = "blocked";
-									show = false;
-								}
-				 			}
-				 		}
-				 		Util.log(packageName + " (" + appName + ")\n\thaystack: " + content + "\n\tneedles (all): " + all + "\n\tneedles (" + packageName + "): " + fPackage + "\n\ttoast: " + blocked);
-			 		}
-				} catch (Throwable e) {
-					Util.log(e);
 				}
-		        if (!show) {
-		        	param.setResult(null);
-		        }
-			}
-		};
-    	XposedHelpers.findAndHookMethod(Toast.class, "show", hook);
-        //PATH = startupParam.modulePath;
-    }
-}
+			};
+			Method method = XposedHelpers.findMethodExact(Toast.class, "show");
+			method.setAccessible(true);
+			XposedBridge.hookMethod(method, hook);
+	        //PATH = startupParam.modulePath;
+	    } catch (Throwable e) {
+	    	Util.log(e);
+	    }
+    } // void initZygote(StartupParam startupParam)
+} // class Hook implements IXposedHookLoadPackage, IXposedHookZygoteInit
